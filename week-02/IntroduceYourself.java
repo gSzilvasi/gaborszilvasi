6c1a9f4bfde8338bde678609c3058e374af9708a
Szilvási Gábor Péter <gszilvasi@protonmail.com>
2021-10-20 20:54:10 +0200

    add java projects

---
+++ week-02/IntroduceYourself.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiír rólad pár részletet a konzolra
+        // Minden egyes részletet új sorba írja a program:
+        //  - Neved
+        //  - Korod
+        //  - Magasságod méterben kifejezve (tört érték)        // Egy karakterláncot (string) printel a terminal ablakba.
+        System.out.println("Szilvási Gábor Péter");
+        System.out.println(30);
+        System.out.println(1.8+"m");
+    }
+}
