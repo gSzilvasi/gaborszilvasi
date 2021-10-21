079ccead09df2908ee2e0dcbe29679eaabf6969f
Szilvási Gábor Péter <gszilvasi@protonmail.com>
2021-10-21 23:09:58 +0200

    Swap.java add

---
+++ week-02/Swap.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy;
+
+public class Swap {
+        public static void main(String[] args) {
+            // Cseréld meg a 2 változó értékét programozás útján.
+            // Az a értéke legyen az, ami a b változóban van és fordítva.
+            int a = 123;
+            int b = 526;
+            int c = 403;
+
+            System.out.println(a+c);
+            System.out.println(b-c);
+        }
+}
