98265118ebf61c5622f869799ae4a9e26b0d11c0
Szilvási Gábor Péter <gszilvasi@protonmail.com>
2021-10-22 11:31:29 +0200

    Upload IWontCheatOnTheExams.java

---
+++ week-02/IWontCheatOnTheExams.java
@@ -0,0 +1,11 @@
+package com.greenfoxacademy;
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        // Írj egy programot ami 100-szor kiírja:
+        // "Nem fogok csalni az a vizsgán!"
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Hello World!");
+        }
+    }
+}
