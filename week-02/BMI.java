20de7e4e67ae82b794a316c3a00f5bd0ed15a725
Szilvási Gábor Péter <gszilvasi@protonmail.com>
2021-10-25 18:56:13 +0200

    Update BMI.java file

--- week-02/BMI.java
+++ week-02/BMI.java
@@ -1,14 +1,15 @@
 package com.greenfoxacademy;
 
 public class BMI {
-        public static void main(String[] args) {
-            double massInKg = 81.2;
-            double heightInM = 1.78;
-            double heightInMSquared = heightInM * heightInM;
- //         System.out.println(massInKg / heightInMSquared);
-            System.out.println(massInKg/(heightInM * heightInM));
-            // Számíttasd és írasd ki a testtömeg-indexet (BMI)
-            // a változókat használva
-        }
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+        double heightInMSquared = heightInM * heightInM;
+        //         System.out.println(massInKg / heightInMSquared);
+        // Számíttasd és írasd ki a testtömeg-indexet (BMI)
+        // a változókat használva
+        System.out.println(massInKg / (heightInM * heightInM));
+
+    }
 }
 // TTI= testtömeg (kg)/magasság m2
