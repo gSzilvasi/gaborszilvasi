d83193ac895d064762c6782aeb6f00d34736d4ab
Szilvási Gábor Péter <gszilvasi@protonmail.com>
2021-10-25 18:09:04 +0200

    Upload LipemLopom.java new version

--- week-02/lipemLopom.java
+++ week-02/lipemLopom.java
@@ -3,9 +3,15 @@ package com.greenfoxacademy;
 class LipemLopom {
     public static void main(String[] args) {
         // Írasd ki a "Lipem lopom a szőlőt" című mondóka sorait helyes sorrendben
-        System.out.println("Lipem, lopom a szőlőt,");
-        System.out.println("elaludt az öreg csősz.");
-        System.out.println("Furkósbot a kezébe,");
-        System.out.println("vaskalap a fejébe.");
+
+        String a = "Lipem, lopom a szőlőt,";
+        String b = "elaludt az öreg csősz.";
+        String c = "Furkósbot a kezébe,";
+        String d = "vaskalap a fejébe.";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
