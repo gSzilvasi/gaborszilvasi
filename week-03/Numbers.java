b9631b83a1359574bc175627bb86e173b06b23f3
Szilvási Gábor Péter <gszilvasi@protonmail.com>
2021-10-26 14:49:13 +0200

    Update Arrays/Numbers.java file

---
+++ week-03/Numbers.java
@@ -0,0 +1,9 @@
+public class Numbers {
+    public static void main(String[] args) {
+        // - Készíts egy `numbers` nevű változót (tömböt)
+        //   a következő tartalommal (értékekkel): `[4, 5, 6, 7]`
+        // - Írasd ki a konzolra a `numbers` nevű tömb harmadik elemét
+        int[] tomb = new int[]{4, 5, 6, 7};
+        System.out.print(tomb[2]);
+    }
+}
